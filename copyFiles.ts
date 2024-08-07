8b09f9c6c32c8fd89783ce888b1c64c290f1366d
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-08-07 18:58:38 +0800

    feat: update copyFiles

--- copyFiles.ts
+++ copyFiles.ts
@@ -1,9 +1,24 @@
 import fs from 'fs-extra';
 import { logger } from './src/utils';
+import path from 'path';
+
+const originDbJsonPath = path.join(__dirname, 'src/resources/dbConfigJson/index.json');
+const targetDbJsonPath = path.join(__dirname, 'bin/resources/dbConfigJson/index.json');
+
+const filter = (src: string, dest: string) => {
+  if (src.includes('dbConfigJson')) {
+    return false;
+  }
+  return true;
+};
+
 const copyFiles = async () => {
   try {
-    await fs.copy('src/resources', 'bin/resources');
+    await fs.copy('src/resources', 'bin/resources', { filter });
     await fs.copy('src/config.json', 'bin/config.json');
+    if (!fs.existsSync(targetDbJsonPath)) {
+      await fs.copy(originDbJsonPath, targetDbJsonPath);
+    }
     logger.info('Files copied successfully!');
   } catch (err) {
     logger.error('Error copying files:', err);
