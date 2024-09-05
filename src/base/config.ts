4909edb8a0c35976af4e0227caa1d2a5c7a045a6
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-09-05 10:12:28 +0800

    fix: cli config command

--- src/base/config.ts
+++ src/base/config.ts
@@ -1,17 +1,24 @@
 import { dbService } from '../services';
 import chalk from 'chalk';
 import { logger } from '../utils';
+import { init } from '../plugins/loadConfig';
+import { PluginContext, gitSeverPath } from '../shared';
 
-module.exports = async (value: any, cfg: Record<keyof any, any>) => {
-  logger.info(`${chalk.cyan(`value :`)}`, value);
-  logger.info(`${chalk.cyan(`cfg : `)}`, cfg);
-  if (!Object.keys(cfg).length) return;
-  if (cfg.set && value) {
-    dbService.gitServerConfigDb.set(cfg.set, value);
+module.exports = async (value: any, options: Record<keyof any, any>) => {
+  init({} as PluginContext);
+  logger.info(`${chalk.cyan(`value`)} : ${chalk.green(value)}`);
+  logger.info(`${chalk.cyan(`options`)} : ${chalk.green(JSON.stringify(options))}`);
+  if (!Object.keys(options).length) return;
+  if (options.set && value) {
+    dbService.gitServerConfigDb.set(options.set, value);
     await dbService.writeGitServerConfig();
-    logger.info(`${chalk.cyan(`${cfg.set} : ${value}`)}`);
+    logger.info(`set file [${gitSeverPath}] the field ${chalk.cyan(options.set)} to ${chalk.green(value)}`);
   }
-  if (cfg.get) {
-    logger.info(`${chalk.cyan(`${cfg.get} : ${dbService.gitServerConfigDb.get(cfg.get)}`)}`);
+  if (options.get) {
+    logger.info(
+      `get file [${gitSeverPath}] the field ${chalk.cyan(options.get)} as ${chalk.green(
+        dbService.gitServerConfigDb.get(options.get)
+      )}`
+    );
   }
 };
