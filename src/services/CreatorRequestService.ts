6199b52cca2521fd3548ddedf368b2f8aebfc42e
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-10-26 10:59:00 +0800

    feat: add fetchAllData for git server

--- src/services/CreatorRequestService.ts
+++ src/services/CreatorRequestService.ts
@@ -2,28 +2,48 @@ import { dbService } from '../services';
 import { axios } from '../utils';
 import { GITSERVER, Repo } from '../shared';
 
-const fetchRepoList = async () => {
-  const { origin, orgs, user } = await dbService.readGitServerConfig();
-  return axios.get(`${origin}/${orgs ? 'orgs' : 'users'}/${orgs ? orgs : user}/repos`);
+const buildRepoUrl = (origin: string, orgs: string, user: string) =>
+  `${origin}/${orgs ? 'orgs' : 'users'}/${orgs || user}/repos`;
+
+const fetchAllData = async (url: string, params: Record<keyof any, any> = {}) => {
+  const allData = [];
+  let page = 1;
+  let hasMore = true;
+  while (hasMore) {
+    const data = await axios.get<typeof params, Record<keyof any, any>[]>(url, { params: { ...params, page } });
+    allData.push(...data);
+    hasMore = data.length === params.per_page;
+    page++;
+  }
+  return allData;
 };
 
-const fetchTagList = async (repo: Repo) => {
+const fetchGithubRepoList = async (params = { per_page: 100 }) => {
+  const { origin, orgs, user } = await dbService.readGitServerConfig();
+  const url = buildRepoUrl(origin, orgs, user);
+  return await fetchAllData(url, params);
+};
+
+const fetchGithubTagList = async (repo: Repo) => {
   const { origin, orgs, user } = await dbService.readGitServerConfig();
   return axios.get(`${origin}/repos/${orgs ? orgs : user}/${repo.name}/tags`);
 };
 
-const fetchGitLabRepoList = async () => {
+const fetchGitLabRepoList = async (params = { per_page: 100 }) => {
   const { origin } = await dbService.readGitServerConfig();
-  return axios.get(`${origin}/api/v4/projects`);
+  const url = `${origin}/api/v4/projects`;
+  return await fetchAllData(url, params);
 };
+
 const fetchGitLabTagList = async (repo: Repo) => {
   const { origin } = await dbService.readGitServerConfig();
   return axios.get(`${origin}/api/v4/projects/${repo.id}/repository/tags`);
 };
+
 export default {
   [GITSERVER.GITHUB]: {
-    fetchTagList,
-    fetchRepoList
+    fetchTagList: fetchGithubTagList,
+    fetchRepoList: fetchGithubRepoList
   },
   [GITSERVER.GITLAB]: {
     fetchTagList: fetchGitLabTagList,
