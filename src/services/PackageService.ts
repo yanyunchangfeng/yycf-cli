a5d0ae28cecb8c57809893ba11c30244abd533a3
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-20 11:42:40 +0800

    feat: update writePkg plugin

--- src/services/PackageService.ts
+++ src/services/PackageService.ts
@@ -1,6 +1,7 @@
 import { PluginContext } from '../shared';
 import { readFile, writeFile } from '../utils';
 import path from 'path';
+import url, { URL } from 'url';
 class PackageService {
   context;
   constructor(context: PluginContext) {
@@ -11,6 +12,12 @@ class PackageService {
     const pInfo: any = await readFile(pkgPath, true);
     if (!pInfo) return;
     pInfo.name = this.context.projectName;
+    const repoUrl = pInfo.repository?.url;
+    if (repoUrl) {
+      const parsedUrl = new URL(repoUrl);
+      const baseUrl = path.join(parsedUrl.origin, parsedUrl.pathname.split('/').filter(Boolean).slice(0, 1).join('/'));
+      pInfo.repository.url = path.join(baseUrl, `${pInfo.name}.git`);
+    }
     await writeFile(pkgPath, pInfo, true);
   }
   async init() {
